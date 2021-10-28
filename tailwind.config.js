780d885da611b115255c66f13c595e5249a0eb2a
dewolaj881 <dewolaj881@gmail.com>
2021-10-28 15:51:15 +0200

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'russo': ['Russo One', 'sans-serif']
+      },
+      spacing: {
+        'spacing-mobile': '500px'
+      },
+      backgroundImage: theme => ({
+        'hero-pattern': "url('/img/background.jpg')",
+        'interchange': "url('/img/interchange-min.jpg')",
+        'highway': "url('/img/highway-min.jpg')",
+        'road': "url('/img/road-min.jpg')",
+      }),
+
+    },
   },
   variants: {
     extend: {},
