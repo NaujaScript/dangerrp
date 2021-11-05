43f4ceab172046d07a6ca249f5d5986a9c7894f9
dewolaj881 <dewolaj881@gmail.com>
2021-11-05 18:35:49 +0100

    fix: class to className

--- components/Ad.js
+++ components/Ad.js
@@ -6,9 +6,9 @@ import React from "react";
 function Ad() {
   return (
     <div>
-      <div class="text-white font-bold items-center block md:flex">
+      <div className="text-white font-bold items-center block md:flex">
         <a href="/DangerRP.pdf" className="flex flex-col m-auto bg-yellow-600 h-64 md:w-3/4 w-full hover:transition hover:duration-300 hover:bg-yellow-500">
-          <img class="m-auto h-24 md:h-32" src="/img/rules.png" alt="" />
+          <img className="m-auto h-24 md:h-32" src="/img/rules.png" alt="" />
           <p className="m-auto font-bold text-2xl">Regulamin</p>
         </a>
         <a href="http://dc.dangerrp.pl" className="flex flex-col m-auto bg-yellow-600 h-64 md:w-1/4 w-full hover:transition hover:duration-300 hover:bg-yellow-500">
--- components/Showcase.js
+++ components/Showcase.js
@@ -6,13 +6,13 @@ import React from "react";
 function Showcase() {
   return (
     <div>
-      <div class="text-white font-bold bg-cover bg-no-repeat  bg-center h-spacing-mobile md:h-screen -mt-10 md:-mt-36 items-center bg-hero-pattern">
+      <div className="text-white font-bold bg-cover bg-no-repeat  bg-center h-spacing-mobile md:h-screen -mt-10 md:-mt-36 items-center bg-hero-pattern">
         <div className="block mx-auto md:mx-px">
           <h1 className="pt-36 md:pt-96 text-4xl flex justify-center  md:text-left text-center md:text-6xl">DANGER Roleplay</h1>
-          <button class="md:flex hidden m-auto justify-items-center justify-self-center bg-black bg-opacity-60 border-2 border-yellow-400 z-30 mb-36 py-4 px-8  mt-8 hover:bg-gray-900 hover:border-2 hover:border-white hover:text-white hover:animate-bounce hover:transition hover:duration-300 hover:delay-300">
+          <button className="md:flex hidden m-auto justify-items-center justify-self-center bg-black bg-opacity-60 border-2 border-yellow-400 z-30 mb-36 py-4 px-8  mt-8 hover:bg-gray-900 hover:border-2 hover:border-white hover:text-white hover:animate-bounce hover:transition hover:duration-300 hover:delay-300">
             <p>Obejrzyj Teaser</p>
           </button>
-          <button class="md:hidden flex m-auto justify-items-center justify-self-center bg-black bg-opacity-60 border-2 border-yellow-400 z-30 mb-36 py-4 px-8  mt-8 hover:bg-gray-900 hover:border-2 hover:border-white hover:text-white hover:animate-bounce">
+          <button className="md:hidden flex m-auto justify-items-center justify-self-center bg-black bg-opacity-60 border-2 border-yellow-400 z-30 mb-36 py-4 px-8  mt-8 hover:bg-gray-900 hover:border-2 hover:border-white hover:text-white hover:animate-bounce">
             <p>Obejrzyj Teaser</p>
           </button>
         </div>
